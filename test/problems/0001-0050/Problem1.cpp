94db73d887719ea36274510f93e4afba2a7eb4b0
Will Mitchell <will@wmitchell.net>
2013-10-08 17:48:42 -0600

    Add check via Factory for Problem1

--- test/problems/0001-0050/Problem1.cpp
+++ test/problems/0001-0050/Problem1.cpp
@@ -2,16 +2,26 @@
 
 #include "problems/0001-0050/Problem1.h"
 using problems::Problem1;
+#include "problems/Factory.h"
+using problems::Factory::create;
 
 TEST(Problem1, Basics) {
+  Problem1 p;
 
-  Problem1 p1;
+  EXPECT_EQ(23, p.brute_force(10));
+  EXPECT_EQ(233168, p.brute_force(1000));
+  EXPECT_EQ(23, p.faster(10));
+  EXPECT_EQ(233168, p.faster(1000));
+  EXPECT_EQ(p.brute_force(1000), p.faster(1000));
+  EXPECT_EQ(p.brute_force(100), p.faster(100));
+  EXPECT_EQ(p.brute_force(10), p.faster(10));
+}
 
-  EXPECT_EQ(23, p1.brute_force(10));
-  EXPECT_EQ(233168, p1.brute_force(1000));
-  EXPECT_EQ(23, p1.faster(10));
-  EXPECT_EQ(233168, p1.faster(1000));
-  EXPECT_EQ(p1.brute_force(1000), p1.faster(1000));
-  EXPECT_EQ(p1.brute_force(100), p1.faster(100));
-  EXPECT_EQ(p1.brute_force(10), p1.faster(10));
+TEST(Problem1, ViaFactory) {
+  auto p = create(1);
+
+  p->solve();
+
+  EXPECT_EQ("The sum of all multiples of 3 or 5 below 1000 is 233168",
+	    p->answer());
 }
