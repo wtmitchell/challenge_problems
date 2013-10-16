f8f41218b5646f61b1353cc4e3f35f85b52bb167
Will Mitchell <will@wmitchell.net>
2013-10-15 23:33:04 -0600

    Add problem 7

---
+++ test/problems/0001-0050/Problem7.cpp
@@ -0,0 +1,66 @@
+#include <gtest/gtest.h>
+
+#include "problems/Factory.h"
+using problems::Factory::create;
+
+#include "problems/0001-0050/Problem7.h"
+using problems::Problem7;
+
+TEST(Problem7, Basics) {
+  Problem7 p;
+
+  EXPECT_EQ(p.bruteForce(100), p.bruteForce2(100));
+  EXPECT_EQ(p.bruteForce(100), p.bruteForce3(100));
+}
+
+TEST(Problem7, bruteForce) {
+  Problem7 p;
+
+  EXPECT_EQ(13, p.bruteForce(6));
+  EXPECT_EQ(29, p.bruteForce(10));
+  EXPECT_EQ(541, p.bruteForce(100));
+  EXPECT_EQ(7919, p.bruteForce(1000));
+  EXPECT_EQ(104729, p.bruteForce(10000));
+  EXPECT_EQ(104743, p.bruteForce(10001));
+}
+
+TEST(Problem7, bruteForce2) {
+  Problem7 p;
+
+  EXPECT_EQ(13, p.bruteForce2(6));
+  EXPECT_EQ(29, p.bruteForce2(10));
+  EXPECT_EQ(541, p.bruteForce2(100));
+  EXPECT_EQ(7919, p.bruteForce2(1000));
+  EXPECT_EQ(104729, p.bruteForce2(10000));
+  EXPECT_EQ(104743, p.bruteForce2(10001));
+}
+
+TEST(Problem7, bruteForce3) {
+  Problem7 p;
+
+  EXPECT_EQ(13, p.bruteForce3(6));
+  EXPECT_EQ(29, p.bruteForce3(10));
+  EXPECT_EQ(541, p.bruteForce3(100));
+  EXPECT_EQ(7919, p.bruteForce3(1000));
+  EXPECT_EQ(104729, p.bruteForce3(10000));
+  EXPECT_EQ(104743, p.bruteForce3(10001));
+}
+
+TEST(Problem7, bruteForce4) {
+  Problem7 p;
+
+  EXPECT_EQ(13, p.bruteForce4(6));
+  EXPECT_EQ(29, p.bruteForce4(10));
+  EXPECT_EQ(541, p.bruteForce4(100));
+  EXPECT_EQ(7919, p.bruteForce4(1000));
+  EXPECT_EQ(104729, p.bruteForce4(10000));
+  EXPECT_EQ(104743, p.bruteForce4(10001));
+}
+
+TEST(Problem7, ViaFactory) {
+  auto p = create(7);
+
+  p->solve();
+
+  EXPECT_EQ("The 10001st prime number is 104743", p->answer());
+}
