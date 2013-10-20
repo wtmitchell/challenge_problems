f16d08df9b171779d4dc096f790517b4df71bdc6
Will Mitchell <will@wmitchell.net>
2013-10-20 01:15:42 -0600

    Add numbers::divisors and numbers::divisorsUnsorted with tests

---
+++ include/number/Divisor.h
@@ -0,0 +1,68 @@
+//===-- number/Divisor.h ----------------------------------------*- C++ -*-===//
+//
+// ProjectEuler.net solutions by Will Mitchell
+//
+// This file is distributed under the MIT License. See LICENSE for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief Contains functions relating to divisors
+///
+//===----------------------------------------------------------------------===//
+#ifndef NUMBER_DIVISOR_H_INCLUDED
+#define NUMBER_DIVISOR_H_INCLUDED
+
+#include <algorithm>
+#include <vector>
+
+namespace number {
+/// Returns a vector of the divisors of the argument. Returned vector will be
+/// sorted in increasing order.
+template <class T> std::vector<T> divisors(const T n);
+
+/// Returns a vector of the divisors of the argument. Returned vector will not
+/// be sorted
+template <class T> std::vector<T> divisorsUnsorted(const T n);
+}
+
+template <class T> std::vector<T> number::divisors(const T n) {
+  // Initially will contain divisors at most the sqrt
+  std::vector<T> divisors;
+  // Will contain divisors greater than the sqrt
+  std::vector<T> big_divisors;
+
+  // Find the divisors
+  for(auto i = static_cast<T>(1); i * i <= n; ++i) {
+    if (n % i == 0) {
+      // Divisors come in pairs
+      divisors.push_back(i);
+      big_divisors.push_back(n / i);
+    }
+  }
+
+  // Add the big_divisors to divisors
+  divisors.reserve(divisors.size() * 2);
+  for (auto i = big_divisors.rbegin(), e = big_divisors.rend(); i != e; ++i)
+    divisors.push_back(*i);
+
+  return divisors;
+}
+
+template <class T> std::vector<T> number::divisorsUnsorted(const T n) {
+  std::vector<T> divisors;
+
+  // Find the divisors
+  for(auto i = static_cast<T>(1); i * i <= n; ++i) {
+    if (n % i == 0) {
+      // Divisors come in pairs
+      divisors.push_back(i);
+      divisors.push_back(n / i);
+    }
+  }
+
+  return divisors;
+}
+
+
+#endif
---
+++ test/number/Divisor.cpp
@@ -0,0 +1,45 @@
+#include <gtest/gtest.h>
+
+#include <vector>
+using std::vector;
+
+#include "number/Divisor.h"
+using number::divisors;
+using number::divisorsUnsorted;
+
+TEST(Divisor, divisor) {
+  vector<unsigned> expected;
+  expected.push_back(1);
+  expected.push_back(2);
+  expected.push_back(3);
+  expected.push_back(6);
+
+  EXPECT_EQ(expected, divisors(6u));
+
+  expected.push_back(9);
+  expected.push_back(18);
+
+  EXPECT_EQ(expected, divisors(18u));
+
+}
+
+TEST(Divisor, divisorUnsorted) {
+  vector<unsigned> expected;
+  expected.push_back(1);
+  expected.push_back(6);
+  expected.push_back(2);
+  expected.push_back(3);
+
+  EXPECT_EQ(expected, divisorsUnsorted(6u));
+
+  expected.clear();
+  expected.push_back(1);
+  expected.push_back(18);
+  expected.push_back(2);
+  expected.push_back(9);
+  expected.push_back(3);
+  expected.push_back(6);
+
+  EXPECT_EQ(expected, divisorsUnsorted(18u));
+
+}
