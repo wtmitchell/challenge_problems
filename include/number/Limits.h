9d1e94e69f70fb039b7da4dfaa5a753d98044be3
Will Mitchell <will@wmitchell.net>
2013-10-22 18:07:59 -0600

    number::maxSqrt now correctly works for signed types

--- include/number/Limits.h
+++ include/number/Limits.h
@@ -13,6 +13,7 @@
 #ifndef NUMBER_LIMITS_H_INCLUDED
 #define NUMBER_LIMITS_H_INCLUDED
 
+#include <cstddef>
 #include <type_traits>
 
 namespace number {
@@ -23,28 +24,58 @@ template <typename T> T maxSqrt();
 namespace number {
 template <typename T> struct maxSqrtUnsigned {
   const static T value =
-      (static_cast<T>(1) << sizeof(T) * 4) - static_cast<T>(1);
+      static_cast<T>((static_cast<T>(1) << sizeof(T) * 4) - static_cast<T>(1));
+};
+
+template <std::size_t size> struct maxSqrtSignedImpl {
+  // Hopefully zero will be wrong enough to immediately make the program fail
+  enum {
+    value = 0
+  };
+};
+
+// 8-bit
+template <> struct maxSqrtSignedImpl<1> {
+  // floor(sqrt(2^7 - 1))
+  enum {
+    value = 11
+  };
+};
+
+// 16-bit
+template <> struct maxSqrtSignedImpl<2> {
+  // floor(sqrt(2^15 - 1))
+  enum {
+    value = 181
+  };
+};
+
+// 32-bit
+template <> struct maxSqrtSignedImpl<4> {
+  // floor(sqrt(2^32 - 1))
+  enum {
+    value = 46340
+  };
+};
+
+// 64-bit
+template <> struct maxSqrtSignedImpl<8> {
+  // floor(sqrt(2^63 - 1))
+  enum {
+    value = 3037000499
+  };
+};
+
+// 128-bit
+template <> struct maxSqrtSignedImpl<16> {
+  // floor(sqrt(2^127 - 1))
+  enum {
+    value = 13043817825332782212u
+  };
 };
 
 template <typename T> struct maxSqrtSigned {
-  // Hopefully will break things quickly and obviously for this unsupported type
-  const static T value = 0;
-};
-
-template <> struct maxSqrtSigned<short> {
-  const static short value = 181;
-};
-
-template <> struct maxSqrtSigned<int> {
-  const static int value = 46340;
-};
-
-template <> struct maxSqrtSigned<long> {
-  const static long value = 3037000499l;
-};
-
-template <> struct maxSqrtSigned<long long> {
-  const static long long value = 3037000499ll;
+  const static T value = maxSqrtSignedImpl<sizeof(T)>::value;
 };
 
 template <typename T> struct maxSqrtImpl {
