ff404cab7ad15d048a58892c19a468924764deea
Will Mitchell <will@wmitchell.net>
2013-10-26 10:54:10 -0600

    ADd bruteForce solution to Problem 12 and notes about making it faster

---
+++ test/problems/0001-0050/Problem12.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+
+#include "problems/Factory.h"
+using problems::Factory::create;
+
+#include "problems/0001-0050/Problem12.h"
+using problems::Problem12;
+
+TEST(Problem12, Basics) {
+  Problem12 p;
+
+  EXPECT_EQ(28, p.bruteForce(5));
+  EXPECT_EQ(76576500, p.bruteForce(500));
+
+}
+
+TEST(Problem12, ViaFactory) {
+  auto p = create(12);
+
+  p->solve();
+
+  EXPECT_EQ("The 12375th triangular number is the first triangular number with "
+            "over 500 divisors. It is 76576500",
+            p->answer());
+}
